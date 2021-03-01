ab057997e92ff0d4689d92a35fef9a0f4592620c
Răzvan Backpack <razvan.truta@outlook.com>
2021-03-01 19:03:21 +0200

    initial commit

---
+++ TCP_Client/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Sockets;
+using System.Net;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace TCPClient
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            //======================================================================
+            //Server variables declaration
+            //----------------------------------------------------------------------
+            var server_IP = "127.0.0.1";
+            var server_Port = 50000;
+            NetworkStream netStream;
+            BinaryReader bReader;
+            BinaryWriter bWriter;
+            //======================================================================
+
+            try
+            {
+                //======================================================================
+                //Connect and tell us that you are connected, pls!
+                //----------------------------------------------------------------------
+                TcpClient client = new TcpClient();
+                client.Connect(server_IP, server_Port);
+                Console.WriteLine(">>> Client connected to the server [" + server_IP + ":" + server_Port + "]");
+                //======================================================================
+
+
+                //======================================================================
+                //Set up streams and make sure that you can send and receive data from the server
+                //----------------------------------------------------------------------
+                netStream = client.GetStream();
+                bReader = new BinaryReader(netStream);
+                bWriter = new BinaryWriter(netStream);
+
+                if (netStream.CanWrite)
+                {
+                    Console.WriteLine("> You can send data to the server!");
+                }
+                if (netStream.CanRead)
+                {
+                    Console.WriteLine("> You can read data from the server!");
+                }
+                //======================================================================
+
+                Console.WriteLine();
+                Console.WriteLine();
+
+                //======================================================================
+                //Send 0 so the server knows we want to see files in a directory
+                //----------------------------------------------------------------------
+                Console.WriteLine("--------- ListContent call ----------");
+                var directory = "F:\\visual studio\\TCP_Server_Client";
+                bWriter.Write(0);
+                bWriter.Write(directory);
+                var text = bReader.ReadInt32();
+                if(text == 0)
+                {
+                    Console.WriteLine(">>> No file found in the directory. Maybe it does not exist?");
+                }
+                while (text > 0)
+                {
+                    var file = bReader.ReadString();
+                    Console.WriteLine(file);
+                    text--;
+                }
+                //======================================================================
+
+                //Just some empty space visiting us to say hello! :)
+                Console.WriteLine();
+                Console.WriteLine();
+
+
+                //======================================================================
+                //Send 1 to getfile
+                //----------------------------------------------------------------------
+                Console.WriteLine("--------- GetFile call ----------");
+                
+                var filePath = "F:\\yes.jpg";
+                var newFilePath = "F:\\yes2.jpg";
+                bWriter.Write(1);
+                bWriter.Write(filePath);
+                var chunkSize = bReader.ReadInt32();
+                Console.WriteLine("Chunk size received: " + chunkSize);
+                byte[] buffer = new byte[chunkSize];
+
+               // var receivedBytes = 0;
+
+                long fileSize = bReader.ReadInt64();
+                Console.WriteLine("File size received: " + fileSize);
+                using (Stream targetFile = new FileStream(newFilePath, FileMode.OpenOrCreate))
+                {
+                    while (fileSize > 0)
+                    {
+                        int receivedBytes = (int)Math.Min(fileSize, (long)chunkSize);
+                        //Console.WriteLine(receivedBytes);
+                        buffer = bReader.ReadBytes(receivedBytes);
+                        targetFile.Write(buffer, 0, receivedBytes);
+                        fileSize -= receivedBytes;
+                        //Console.WriteLine(fileSize);
+                    }
+                }
+                Console.WriteLine("File transfer complete!");
+                //======================================================================
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception error in main:");
+                Console.WriteLine($"{e}");
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TCP_Server/Program.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using System.IO;
+using System.Threading;
+
+/*
+ *  Homework: GetFile & Client
+ *  Read blocks, write in on the string, flush, rinse & repeat
+ *  Teacher email flavius.stoichescu@microsoft.com
+ * DONT SNED MEMES
+*/
+namespace cicaCevaTCPIPStuff
+{
+    enum RequestType
+    {
+        ListContent = 0,
+        GetFile
+    }
+
+    class Program
+    {
+        static string GetFileMethod(String path)
+        {
+            string fileContent = "";
+            try
+            {
+                fileContent = File.ReadAllText(@path);
+            }
+            catch (Exception e)
+            {
+                return ">>> There was an error opening the file. The given path does not exist or the file cannot be accessed!";
+            }
+
+            return fileContent;
+        }
+        static List<string> ListFolderContent(String path)
+        {
+            try
+            {
+                var result = new List<string>();
+                result.AddRange(Directory.GetFiles(path));
+                foreach (var dir in Directory.GetFiles(path))
+                {
+                    result.AddRange(ListFolderContent(dir));
+
+                }
+                return result;
+            }
+            catch (Exception e)
+            {
+                return new List<string>();
+            }
+        }
+
+        static void ClientWorkerThreadMethod(object c)
+        {
+            try
+            {
+                TcpClient client = (TcpClient)c;
+                Console.WriteLine("> Connection Received!");
+                var clientStream = client.GetStream();
+                //BinaryReader streamReader = new BinaryReader(clientStream);
+                //BinaryWriter streamWriter = new BinaryWriter(clientStream);
+                var streamReader = new BinaryReader(clientStream);
+                var streamWriter = new BinaryWriter(clientStream);
+
+                var reqType = RequestType.ListContent;
+                while (client.Connected)
+                {
+                    reqType = (RequestType)streamReader.ReadInt32();
+                    switch (reqType)
+                    {
+                        case RequestType.ListContent:
+                            Console.WriteLine();
+                            Console.WriteLine();
+                            Console.WriteLine("> ListContent RequestType received!");
+                            var path = streamReader.ReadString();
+                            var files = ListFolderContent(path);
+                            streamWriter.Write(files.Count);
+                            streamWriter.Flush();
+                            foreach (var file in files)
+                            {
+                               
+                                streamWriter.Write(file);
+                            }
+                            streamWriter.Flush();
+                            break;
+                         case RequestType.GetFile:
+                            Console.WriteLine();
+                            Console.WriteLine();
+                            Console.WriteLine("> GetFile RequestType received!");
+                            var filePath = streamReader.ReadString();
+                            
+                            const int chunkSize = 1024; //Read a file in chunks of 1KB
+                           
+                            using (var fileContent = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+                            {
+                                
+                                var readBytes = 0;  
+                                streamWriter.Write(chunkSize);
+                                var fileSize = fileContent.Length;
+                                streamWriter.Write(fileSize);
+                                streamWriter.Flush();
+                                while (true)
+                                {
+                                    var buffer = new byte[chunkSize];
+                                    readBytes = fileContent.Read(buffer, 0, buffer.Length);
+                                    
+                                    if (readBytes == 0)
+                                    {
+                                        streamWriter.Write(0);
+                                        break;
+                                    }
+                                    streamWriter.Write(buffer);
+                                    
+                                }
+                            }
+                            Console.WriteLine("> Sending file done!");
+                              
+                            streamWriter.Flush();
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                //streamWriter.Write("go away");
+                //streamWriter.Flush();
+                //var message = streamReader.ReadString();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error in thread");
+                Console.WriteLine($"{e}");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                TcpListener server = new TcpListener(IPAddress.Any, 50000);
+                server.Start();
+                Console.WriteLine(">>> Server Started!");
+                while (server.Server.IsBound)
+                {
+                    var client = server.AcceptTcpClient();
+                    Thread clientWorker = new Thread(ClientWorkerThreadMethod);
+                    clientWorker.Start(client);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error in main.");
+                Console.WriteLine($"{e}");
+            }
+        }
+    }
+}
